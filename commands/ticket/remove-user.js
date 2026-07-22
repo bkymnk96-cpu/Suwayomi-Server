46a3c2016bf85d26fc53507d5d54b11feaa3c701
bkymnk96-cpu <bkymnk96@gmail.com>
2026-07-22 20:02:28 +0300

    Update remove-user.js

--- commands/ticket/remove-user.js
+++ commands/ticket/remove-user.js
@@ -1,4 +1,38 @@
-const { SlashCommandBuilder } = require('discord.js');
-const db = require('../../database/db');
-const { isStaff } = require('../../utils/ticketAdvanced');
-module.exports = { data: new SlashCommandBuilder().setName('remove-user').setDescription('إزالة عضو من قناة التذكرة الحالية').addUserOption(o=>o.setName('user').setDescription('العضو').setRequired(true)), async execute(interaction){ const ticket=db.getTicketByChannel(interaction.channelId); const settings=db.getTicketSettings(interaction.guildId); if(!ticket) return interaction.reply({content:'{emoji:circlex} هذه القناة ليست تذكرة.',flags:['Ephemeral']}); if(!isStaff(interaction.member,settings)) return interaction.reply({content:'{emoji:circlex} هذا الأمر لفريق الدعم فقط.',flags:['Ephemeral']}); const member=interaction.options.getMember('user'); await interaction.channel.permissionOverwrites.edit(member.id,{ViewChannel:false,SendMessages:false}); return interaction.reply({content:`{emoji:userx} تمت إزالة ${member} من التذكرة.`}); } };
+const { SlashCommandBuilder } = require("discord.js");
+const keyValueService = require("../../services/keyValueService");
+const { canManageTicket } = require("../../utils/ticketUtils");
+
+module.exports = {
+    adminsOnly: false,
+    data: new SlashCommandBuilder()
+        .setName('remove-user')
+        .setDescription('إزالة عضو من قناة التذكرة الحالية')
+        .addUserOption(option => 
+            option
+                .setName('user')
+                .setDescription('اختر العضو لإزالته')
+                .setRequired(true)
+        ),
+    
+    /**
+     * @param { import('discord.js').ChatInputCommandInteraction } interaction 
+     */
+    async execute(interaction) {
+        const ticketData = await keyValueService.get('ticketDB', `TICKET-PANEL_${interaction.channel.id}`);
+        if (!ticketData) {
+            return interaction.reply({ content: `> هذه القناة ليست تذكرة`, ephemeral: true });
+        }
+
+        if (!canManageTicket(interaction.member, ticketData)) {
+            return interaction.reply({ content: `{emoji:circlex} لا تمتلك صلاحية إزالة أعضاء من هذه التذكرة.`, ephemeral: true });
+        }
+
+        const member = interaction.options.getMember('user');
+        await interaction.channel.permissionOverwrites.edit(member.user.id, {
+            ViewChannel: false,
+            SendMessages: false
+        });
+
+        return interaction.reply({ content: `{emoji:circlecheck} تمت إزالة ${member} من التذكرة ${interaction.channel}.` });
+    }
+};
